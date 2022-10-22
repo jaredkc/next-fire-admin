1ebf03ed5f47f8d21a2f80c2d639e7a3f942d953
jaredkc <jared@clivecoffee.com>
2022-10-22 10:05:34 -0600

    remove footer and vercel logo

--- pages/about.tsx
+++ pages/about.tsx
@@ -22,18 +22,6 @@ const About: NextPage = () => {
           .
         </p>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -29,18 +29,6 @@ const Home: NextPage = () => {
           for routing example.
         </p>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
   );
 };
