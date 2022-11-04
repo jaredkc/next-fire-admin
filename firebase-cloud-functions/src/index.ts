a2e9314f3ee002693c099e067cac73f54dc4a7bb
jaredkc <jared@clivecoffee.com>
2022-11-03 18:05:43 -0600

    auth function to allow specific domains

--- firebase-cloud-functions/src/index.ts
+++ firebase-cloud-functions/src/index.ts
@@ -7,3 +7,20 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+// Only users of a specific domain can sign up.
+
+exports.beforeCreate = functions.auth.user().beforeCreate((user, context) => {
+  const allowed = ["@acme.com", "@acme.org"];
+
+  const isAllowed = (email: string) => {
+    return allowed.some((domain) => email.endsWith(domain));
+  };
+
+  if (user.email && !isAllowed(user.email)) {
+    throw new functions.auth.HttpsError(
+        "invalid-argument",
+        `Unauthorized email "${user.email}"`
+    );
+  }
+});
